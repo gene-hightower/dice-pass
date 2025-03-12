1f6e31e62dfee2498c58a0d8237c9055549a8ce1
Gene Hightower <gene@digilicious.com>
2025-03-12 16:22:53 -0700

    update rev

--- package.nix
+++ package.nix
@@ -11,8 +11,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "gene-hightower";
     repo = "dicepass";
-    rev = "";
-    sha256 = "";
+    rev = "47e9206e4b446f75b239eca339bbbb671c83a761";
+    sha256 = "0blyzxjmblwn2yqgwq0y2428dh058y51f93rc5fh35fn05s9fx0w";
   };
 
   buildInputs = [ boost ];
